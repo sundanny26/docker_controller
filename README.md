10907106db9d641d626e3d38ec096dca71565794
Sunil Jimenez <16617597+sundanny26@users.noreply.github.com>
2020-05-13 14:14:32 +0000

    add crontab

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 	docker build -t email_c_image .
 
 ## Run Container
-	docker run --name email_controller -v /var/run/docker.sock:/var/run/docker.sock --rm email_c_image
+	docker run -d --name email_controller -v /var/run/docker.sock:/var/run/docker.sock --rm email_c_image
 
--- script.py
+++ script.py
@@ -1,16 +1,28 @@
-import docker, json
+import docker, json, time
 from datetime import datetime
 
 client = docker.from_env()
-containers = [ n for n in client.containers.list() if 'barong_email' in n.name ]
 
-print(containers)
-# 2020-05-12 T 19:11:45 . 973516582Z
-for n in containers:
-	print(n.name)
-	print(n.attrs['Created'])
-	datetime_str = n.attrs['Created'].split('.')[0]
-	datetime_object = datetime.strptime(datetime_str, '%Y-%m-%dT%H:%M:%S')
-	print(datetime_object)
-	#n.stop()
-	n.remove(force=True)
+def main():
+	containers = [ n for n in client.containers.list() if 'barong_email' in n.name ]
+	print(containers)
+	# 2020-05-12 T 19:11:45 . 973516582Z
+	for n in containers:
+		print(n.name)
+		datetime_str = n.attrs['Created'].split('.')[0]
+		datetime_object = datetime.strptime(datetime_str, '%Y-%m-%dT%H:%M:%S')
+		now = datetime.now()
+		time_limit = 20*60 #20 minutes
+		diff_seconds = (now-datetime_object).seconds
+		print("diff: "+ str(diff_seconds/60.0))
+		if diff_seconds >= time_limit:
+			n.remove(force=True)
+			print("Removing container: " + n.name)
+			break
+
+
+if __name__ == "__main__":
+	while True:
+		print("runnning script: " + str(datetime.now()))
+		main()
+		time.sleep(60)

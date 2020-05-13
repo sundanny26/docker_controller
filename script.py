6f8bf487187eadd7bcd02411cdf1cda1cde289bb
Sunil Jimenez <16617597+sundanny26@users.noreply.github.com>
2020-05-13 16:45:03 +0000

    added service rescaling for constant availability

--- script.py
+++ script.py
@@ -4,6 +4,9 @@ from datetime import datetime
 client = docker.from_env()
 
 def main():
+	service = client.services.list( filters= {'name':'exchange_barong_email'})[0]
+	print('service found: ' + service.name)
+
 	containers = [ n for n in client.containers.list() if 'barong_email' in n.name ]
 	print(containers)
 	# 2020-05-12 T 19:11:45 . 973516582Z
@@ -16,9 +19,15 @@ def main():
 		diff_seconds = (now-datetime_object).seconds
 		print("diff: "+ str(diff_seconds/60.0))
 		if diff_seconds >= time_limit:
-			n.remove(force=True)
-			print("Removing container: " + n.name)
-			break
+			print('scaling to 2 so it keeps running: ' + str(service.scale(2)))
+			time.sleep(5)
+			print("Removing oldest containers and creating new ones: " + n.name)
+			s_containers = [ c for c in client.containers.list() if 'barong_email.1.' in c.name ]
+			for c in s_containers: c.remove(force=True)
+			time.sleep(10)
+			service.reload()
+			print('scaling service back to 1: ' + str(service.scale(1)))
+			return
 
 
 if __name__ == "__main__":
